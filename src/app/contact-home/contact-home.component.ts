96b087270ee67ff1b34161d4e0e029ae8b79375f
Matthew Sanner <matthewsannerdev@gmail.com>
2024-11-21 20:04:10 -0600

    demonstrates property and event binding on contact buttons

--- src/app/contact-home/contact-home.component.ts
+++ src/app/contact-home/contact-home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-home',
   imports: [],
   templateUrl: './contact-home.component.html',
-  styleUrl: './contact-home.component.css'
+  styleUrl: './contact-home.component.css',
 })
-export class ContactHomeComponent {
+export class ContactHomeComponent implements OnInit {
+  isUnchanged = true;
 
+  onCancel(event?: KeyboardEvent) {
+    const evtMsg = event
+      ? ' Event target is ' + (event.target as HTMLElement).textContent
+      : '';
+    alert('Cancelled.' + evtMsg);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
