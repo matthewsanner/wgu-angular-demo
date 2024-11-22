bfc77ec690bb5cbf1ede193f40d5578ed1a3a246
Matthew Sanner <matthewsannerdev@gmail.com>
2024-11-21 20:25:08 -0600

    adds projects content and simulated two-way data binding

--- src/app/projects-home/projects-home.component.ts
+++ src/app/projects-home/projects-home.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-projects-home',
   imports: [],
+  selector: 'app-projects-home',
   templateUrl: './projects-home.component.html',
-  styleUrl: './projects-home.component.css'
+  styleUrls: ['./projects-home.component.css'],
 })
-export class ProjectsHomeComponent {
+export class ProjectsHomeComponent implements OnInit {
+  firstName: string = 'William';
+  lastName: string = 'Wallace';
+  phoneNumber: string = '(201) 748-6000';
 
+  setProperty(event: Event) {
+    let target = event.target as HTMLInputElement;
+    let property: string = target.id;
+    switch (property) {
+      case 'firstNameInput':
+        this.firstName = target.value;
+        break;
+      case 'lastNameInput':
+        this.lastName = target.value;
+        break;
+      case 'phoneNumberInput':
+        this.phoneNumber = target.value;
+        break;
+      default:
+        console.warn('No matching ID found!');
+        break;
+    }
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
