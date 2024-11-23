3b4aaa8c7f459e64a146678394bc0e1c91a8df9b
Matthew Sanner <matthewsannerdev@gmail.com>
2024-11-22 19:18:35 -0600

    implements new highlight directive

--- src/app/services-home/highlight.directive.spec.ts
+++ src/app/services-home/highlight.directive.spec.ts
@@ -1,12 +1,12 @@
 import { HighlightDirective } from './highlight.directive';
-import { ElementRef } from '@angular/core';
 
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
-    const mockElementRef: ElementRef = {
+    // Create a mock for ElementRef
+    const mockElementRef = {
       nativeElement: document.createElement('div'),
     };
-    const directive = new HighlightDirective(mockElementRef);
+    const directive = new HighlightDirective(mockElementRef as any);
     expect(directive).toBeTruthy();
   });
 });
